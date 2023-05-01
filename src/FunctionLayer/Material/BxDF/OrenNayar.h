49a914624530396579ab0028b2d1a73917428ff5
xuanxuan <522169030@qq.com>
2023-05-01 16:03:15 +0800

    oren-nayar模型

--- src/FunctionLayer/Material/BxDF/OrenNayar.h
+++ src/FunctionLayer/Material/BxDF/OrenNayar.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "BSDF.h"
 #include "Warp.h"
+#include <cmath>
+#include <cstdio>
 
 class OrenNayarBSDF : public BSDF {
 public:
@@ -9,12 +11,18 @@ public:
       : BSDF(_normal, _tangent, _bitangent), albedo(_albedo), sigma(_sigma) {}
 
   virtual Spectrum f(const Vector3f &wo, const Vector3f &wi) const override {
-    // TODO
     // 1. 转换坐标系到局部坐标
     // 2. 计算 A, B, \alpha, \beta（可以直接求\sin\alpha,\tan\beta）,
     // \cos(\phi_i-\phi_o)
     // 3. return Oren-Nayar brdf
-    return {0.f};
+    Vector3f woLocal = toLocal(wo), wiLocal = toLocal(wi);
+    float theta_o = acos(woLocal[1]), theta_i = acos(wiLocal[1]);
+    float alpha = std::max(theta_o, theta_i), beta = std::min(theta_o, theta_i);
+    float A = 1 - sigma * sigma / (2 * (sigma * sigma + 0.33));
+    float B = 0.45 * sigma * sigma / (sigma * sigma + 0.09);
+    float phi_i = atan2(wiLocal[0], wiLocal[2]), phi_o = atan2(woLocal[0], woLocal[2]);
+    float coeff = 1.0 / M_PI * (A + B * std::max(0.0f, std::cos(phi_i - phi_o)) * std::sin(alpha) * std::tan(beta));
+    return coeff * albedo;
   }
 
   virtual BSDFSampleResult sample(const Vector3f &wo,
